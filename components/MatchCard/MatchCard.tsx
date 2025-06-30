62c359e5b223f1572926e54ebfacce8d2bafbcb5
Jade Bautista <edaj.bautista@gmail.com>
2025-06-30 10:13:17 +0800

    Fix build issue

--- components/MatchCard/MatchCard.tsx
+++ components/MatchCard/MatchCard.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useMemo } from 'react';
+import React from 'react';
 import { Match } from '@models/Match';
 import { STATUS_LABELS, LABEL_COLORS } from '@/constants/status';
 import {
